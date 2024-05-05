d11a2882d67aefb300c7469f5794ff1b6b1c678e
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-05 16:54:18 +0500

    I learn type annotations,dynamic type determination and any data type.

---
+++ index.js
@@ -0,0 +1,19 @@
+/********************
+ * TYPE ANNOTATIONS *
+ ********************/
+let num = 45;
+let str = "Hello World";
+console.log(`My number is ${num}`);
+console.log(`My string is ${str}`);
+/******************************
+ * DYNAMIC TYPE DETERMINATION *
+ ******************************/
+let greeting = "Hello";
+console.log(greeting);
+/*******************************
+ * ANY DATA TYPE *
+ ******************************/
+let variable = 54;
+variable = "John";
+console.log(variable);
+export {};
---
+++ index.ts
@@ -0,0 +1,23 @@
+/********************
+ * TYPE ANNOTATIONS *
+ ********************/
+
+let num:number = 45;
+let str:string = "Hello World";
+console.log(`My number is ${num}`)
+console.log(`My string is ${str}`);
+
+/******************************
+ * DYNAMIC TYPE DETERMINATION *
+ ******************************/
+
+let greeting = "Hello";
+console.log(greeting);
+
+/*******************************
+ * ANY DATA TYPE *
+ ******************************/
+
+let variable:any = 54;
+variable = "John";
+console.log(variable);
